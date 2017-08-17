9cc73a90fc38a0cf90beec76317014ed55e016c8
kenny <411316753@qq.com>
2017-08-17 10:43:59 +0800

    add Interceport functions

--- src/main/java/com/KTest/Config/SpringmvcConfig.java
+++ src/main/java/com/KTest/Config/SpringmvcConfig.java
@@ -1,16 +1,20 @@
 package com.KTest.Config;
 
+import com.KTest.interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.KTest")
-public class SpringmvcConfig {
+public class SpringmvcConfig  extends WebMvcConfigurerAdapter{
 
     @Bean
     public InternalResourceViewResolver viewResolver(){
@@ -22,4 +26,19 @@ public class SpringmvcConfig {
     }
 
 
+    @Override
+    //addResourceLocation 指的是文件放置的目录，addResourceHandler指的是对外暴露的访问路径.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+    }
+
+    public DemoInterceptor demoInterceptor(){
+        return  new DemoInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(demoInterceptor());
+    }
 }
